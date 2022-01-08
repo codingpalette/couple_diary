2e3de96dcffb025a65f3834f481204375805b59f
codingpalette <codingpalette@gmail.com>
2022-01-09 01:05:23 +0900

    Toastify css 적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './routes/MainPage'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
