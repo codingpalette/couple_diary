28190f268d1154ffcb01c0330ad6a36af443a34f
codingpalette <codingpalette@gmail.com>
2022-01-08 22:13:45 +0900

    Button component 작업

---
+++ front/src/components/common/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { colors } from '../../../assets/css/GlobalStyles'
+
+export const ButtonTag = styled.button<{ theme: string; width: string }>`
+  width: ${props => props.width};
+  height: 2rem;
+  font-size: 0.875rem;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  line-height: 1;
+  font-weight: 500;
+  color: ${colors.White};
+  background: ${props =>
+    props.theme === 'primary' ? colors.Purple_400 : props.theme === 'secondary' ? colors.Red_400 : colors.Gray_400};
+
+  &:focus {
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
+  }
+  &:hover {
+    opacity: 0.8;
+  }
+`
