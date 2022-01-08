09d483e851025aecd316bdc6a4345959a34483dd
codingpalette <codingpalette@gmail.com>
2022-01-09 00:06:29 +0900

    axios 설정

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import GlobalStyles from './assets/css/GlobalStyles'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_API_URL
 
 ReactDOM.render(
   <React.StrictMode>
