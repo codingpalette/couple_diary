ee72cd9ee80891c75da64550823c1b86af806542
codingpalette <codingpalette@gmail.com>
2022-01-09 00:57:53 +0900

    Toast 설정

---
+++ front/src/hooks/useToast.ts
@@ -0,0 +1,25 @@
+import { toast } from 'react-toastify'
+
+export const ErrorMessageOpen = (msg: string) => {
+  toast.error(msg, {
+    position: 'top-left',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })
+}
+
+export const SuccessMessageOpen = (msg: string) => {
+  toast.success(msg, {
+    position: 'top-left',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  })
+}
