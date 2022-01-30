b04a14ab120e4ea2cc19335bb5a53a4d81c1cf0a
codingpalette <codingpalette@gmail.com>
2022-01-30 20:54:22 +0900

    main page 수정

--- front/src/routes/MainPage/index.tsx
+++ front/src/routes/MainPage/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import { Content } from './styles'
+import React, { useEffect } from 'react'
+import { ContentBox, Section1, Section2 } from './styles'
 import Header from '../../components/common/Header'
+import iphone from '../../assets/images/iPhone12.png'
+import sample from '../../assets/images/sample.png'
+import Button from '../../components/common/Button'
+import useLoginModalSWR from '../../stores/useLoginModalSWR'
 
 const MainPage = () => {
+  const { data: isActive, mutate: setIsActive } = useLoginModalSWR()
+
+  useEffect(() => {
+    console.log(isActive)
+  }, [isActive])
+
   return (
     <>
       <Header />
-      <Content>MainPage</Content>
+      <ContentBox>
+        <Section1>
+          <p>
+            사랑하는 사람에게 특별한 이벤트를 만들어 보세요.
+            <br />
+            소중한 사람과의 추억을 쉽고 편리하게 보여주는 모바일 서비스, <br />
+            커다와 함께라면 당신의 소중한 나날이 더욱 특별해질 거예요.
+          </p>
+        </Section1>
+        <Section2>
+          <div className="section2_content">
+            <div className="title_box">
+              <h1>
+                연인과 함께 했던, <br />
+                장소를 기억하고 <br />
+                보관하세요
+              </h1>
+            </div>
+            <div className="button_box">
+              <Button onClick={() => setIsActive(true)}>시작하기</Button>
+            </div>
+            <div>
+              <div className="image_box">
+                <img src={iphone} alt="iphone" className="iphone" />
+                {/*<img src={sample} alt="sample" className="content" />*/}
+              </div>
+            </div>
+          </div>
+        </Section2>
+      </ContentBox>
     </>
   )
 }
--- front/src/routes/MainPage/styles.ts
+++ front/src/routes/MainPage/styles.ts
@@ -1,6 +1,113 @@
 import styled from 'styled-components'
+import { colors } from '../../assets/css/GlobalStyles'
 
-export const Content = styled.div`
+export const ContentBox = styled.div`
+  padding-top: 60px;
   width: 100%;
-  background-color: #ccc;
+  height: 100%;
+  box-sizing: border-box;
+`
+export const Section1 = styled.div`
+  background-color: #f9fafb;
+  padding: 200px 20px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+
+  p {
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 1.6;
+    text-align: center;
+    color: ${colors.Title};
+    word-break: keep-all;
+    white-space: pre-wrap;
+
+    @media screen and (max-width: 680px) {
+      font-size: 22px;
+    }
+  }
+`
+
+export const Section2 = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  //box-sizing: border-box;
+  position: relative;
+  padding: 200px 0;
+
+  .section2_content {
+    padding: 0 20px;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+  }
+
+  .title_box {
+    position: relative;
+    z-index: 20;
+    h1 {
+      font-size: 50px;
+      font-weight: 700;
+      line-height: 1.4;
+      color: ${colors.Main};
+    }
+  }
+  .image_box {
+    width: 100%;
+    max-width: 600px;
+    //height: 936px;
+    position: absolute;
+    top: 100px;
+    //right: -104px;
+    right: 0;
+    z-index: 10;
+    overflow: hidden;
+
+    .iphone {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      right: -104px;
+      z-index: 10;
+    }
+
+    .content {
+      position: absolute;
+      top: 50%;
+      right: 22px;
+      width: 57%;
+      height: auto;
+      transform: translateY(-50%);
+    }
+  }
+
+  .button_box {
+    margin-top: 20px;
+  }
+
+  @media screen and (max-width: 680px) {
+    .title_box {
+      h1 {
+        font-size: 28px;
+      }
+    }
+    .image_box {
+      position: relative;
+      top: 0;
+
+      .iphone {
+        right: 0;
+      }
+
+      .content {
+        right: 21%;
+        //transform: translate(0, 0%);
+      }
+    }
+  }
 `
