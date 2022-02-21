affa9550acf43a53e1ecf37c4385280f73e036f5
codingpalette <codingpalette@gmail.com>
2022-02-21 21:08:50 +0900

    슬라이드 모달 데이터 받기 이벤트

--- front/src/components/diary/SlideModal/index.tsx
+++ front/src/components/diary/SlideModal/index.tsx
@@ -17,9 +17,11 @@ export type SlideModalProps = {
   isActive: boolean
   /** 모달 닫기 **/
   onClickModalClose: (e?: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => void
+  /** 모달 데이타 **/
+  modalData: any
 }
 
-const SlideModal = ({ isActive, onClickModalClose }: SlideModalProps) => {
+const SlideModal = ({ isActive, onClickModalClose, modalData }: SlideModalProps) => {
   const prevRef = useRef<HTMLDivElement>(null)
   const nextRef = useRef<HTMLDivElement>(null)
 
@@ -38,48 +40,53 @@ const SlideModal = ({ isActive, onClickModalClose }: SlideModalProps) => {
   return (
     <>
       <ModalContainer isActive={isActive} closeEvent={onClickModalClose} maxWidth="500px">
-        <SlideContainer>
-          <h4 className="title">타이틀</h4>
-          <Swiper
-            slidesPerView={1}
-            // navigation={{
-            //   // Both prevEl & nextEl are null at render so this does not work
-            //   prevEl: prevRef.current ? prevRef.current : undefined,
-            //   nextEl: nextRef.current ? nextRef.current : undefined,
-            // }}
-            onSwiper={swiper => {
-              // Delay execution for the refs to be defined
-              setTimeout(() => {
-                // Override prevEl & nextEl now that refs are defined
-                // eslint-disable-next-line no-param-reassign,@typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                swiper.params.navigation.prevEl = prevRef.current
-                // eslint-disable-next-line no-param-reassign,@typescript-eslint/ban-ts-comment
-                // @ts-ignore
-                swiper.params.navigation.nextEl = nextRef.current
+        {modalData && (
+          <SlideContainer>
+            <h4 className="title">{modalData.diaryTitle}</h4>
+            <Swiper
+              slidesPerView={1}
+              // navigation={{
+              //   // Both prevEl & nextEl are null at render so this does not work
+              //   prevEl: prevRef.current ? prevRef.current : undefined,
+              //   nextEl: nextRef.current ? nextRef.current : undefined,
+              // }}
+              onSwiper={swiper => {
+                // Delay execution for the refs to be defined
+                setTimeout(() => {
+                  // Override prevEl & nextEl now that refs are defined
+                  // eslint-disable-next-line no-param-reassign,@typescript-eslint/ban-ts-comment
+                  // @ts-ignore
+                  swiper.params.navigation.prevEl = prevRef.current
+                  // eslint-disable-next-line no-param-reassign,@typescript-eslint/ban-ts-comment
+                  // @ts-ignore
+                  swiper.params.navigation.nextEl = nextRef.current
 
-                // Re-init navigation
-                swiper.navigation.destroy()
-                swiper.navigation.init()
-                swiper.navigation.update()
-              })
-            }}
-            onSlideChange={() => console.log('slide change')}
-          >
-            <div className="navigation prev" ref={prevRef}>
-              <FontAwesomeIcon icon={faAngleLeft} size="3x" />
-            </div>
-            <div className="navigation next" ref={nextRef}>
-              <FontAwesomeIcon icon={faAngleRight} size="3x" />
-            </div>
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-          </Swiper>
-          <div>sdfsdd</div>
-          <Button onClick={onClickModalClose}>닫기</Button>
-        </SlideContainer>
+                  // Re-init navigation
+                  swiper.navigation.destroy()
+                  swiper.navigation.init()
+                  swiper.navigation.update()
+                })
+              }}
+              onSlideChange={() => console.log('slide change')}
+            >
+              <div className="navigation prev" ref={prevRef}>
+                <FontAwesomeIcon icon={faAngleLeft} size="3x" />
+              </div>
+              <div className="navigation next" ref={nextRef}>
+                <FontAwesomeIcon icon={faAngleRight} size="3x" />
+              </div>
+              {modalData.images.map((v: any) => (
+                <SwiperSlide key={v.id}>
+                  <div className="slide_box">
+                    <img src={v.url} alt="img" />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div>sdfsdd</div>
+            <Button onClick={onClickModalClose}>닫기</Button>
+          </SlideContainer>
+        )}
       </ModalContainer>
     </>
   )
--- front/src/components/diary/SlideModal/styles.ts
+++ front/src/components/diary/SlideModal/styles.ts
@@ -8,4 +8,44 @@ export const SlideContainer = styled.div`
     font-size: 1.5rem;
     text-align: center;
   }
+
+  .swiper-container {
+    position: relative;
+  }
+
+  .slide_box {
+    position: relative;
+    &:before {
+      content: '';
+      display: block;
+      width: 100%;
+      padding-bottom: 100%;
+      background-color: #000;
+    }
+    img {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+
+  .navigation {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    z-index: 10;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .prev {
+    left: 10px;
+  }
+
+  .next {
+    right: 10px;
+  }
 `
