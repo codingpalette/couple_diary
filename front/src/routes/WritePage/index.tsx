f3b61f92d2eca0cba44c03df0951e9b1aa77f9ac
codingpalette <codingpalette@gmail.com>
2022-02-25 22:26:36 +0900

    주석 설명 처리

--- front/src/routes/WritePage/index.tsx
+++ front/src/routes/WritePage/index.tsx
@@ -23,57 +23,52 @@ import useUser from '../../hooks/useUser'
 
 const WritePage = () => {
   const { user, isLoading, isError } = useUser()
+
+  // 다이어리 리스트
   const [mapList, setMapList] = useState<any>([])
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setMapList([...mapList, { content: '', position: { lng: 126.987024769656, lat: 37.5705611277251 } }])
-  //   }, 1000)
-  // }, [])
-
-  const [mapInputAddress, onChangeMapInputAddress, onResetMapInputAddress, onSetMapInputAddress] = useInput('')
-  const [addressSearchOpen, setAddressSearchOpen] = useState(false)
-  const [diaryTitle, onChangeDiaryTitle, onResetDiaryTitle] = useInput('')
-  const [contentText, onChangeContentText, onResetContentText] = useInput('')
+  // 달력 상태값
   const [startDate, setStartDate] = useState<Date | null>(new Date())
-
+  // 모달창 주소검색 인풋 상태값
+  const [mapInputAddress, onChangeMapInputAddress, onResetMapInputAddress, onSetMapInputAddress] = useInput('')
+  // 카카오 주소검색 온,오프 상태값
+  const [addressSearchOpen, setAddressSearchOpen] = useState(false)
+  // 주소 검색 후 좌표 상태값
+  const [mapObjData, setMapObjData] = useState<any>({
+    coords: {},
+  })
+  // 다이어리 제목 상태값
+  const [diaryTitle, onChangeDiaryTitle, onResetDiaryTitle] = useInput('')
+  // 이미지 리스트 값
+  const [images, setImages] = useState<any>([])
+  // 내용 상태 값
+  const [contentText, onChangeContentText, onResetContentText] = useInput('')
+  // 마커 생성을 위한 폼 모달 온,오프 상태값
+  const [createModalActive, setCreateModalActive] = useState(false)
+  // 마커 클릭시 나오는 슬라이드 모달 온,오프 상태값
   const [slideModalActive, setSlideModalActive] = useState(false)
-
+  // 슬라이드 모달 닫기 함수
   const onClickSlideModalClose = () => {
     setSlideModalActive(false)
   }
-
-  const [mapObjData, setMapObjData] = useState<any>({
-    fullAddr: '',
-    coords: {},
-  })
-
-  // let map: any
-  // 검색결과 배열에 담아줌
-  const [Places, setPlaces] = useState([])
-
-  const [test, setTest] = useState(false)
-
-  const [isActive, setIsActive] = useState(false)
-
-  const onClickModalOpen = () => {
+  // 마커 생성 폼 모달 오픈 함수
+  const onClickCreateModalOpen = () => {
     onResetDiaryTitle()
     onResetContentText()
     onResetMapInputAddress()
-    setMapObjData({ fullAddr: '', coords: {} })
+    setMapObjData({ coords: {} })
     setImages([])
-    setIsActive(true)
+    setCreateModalActive(true)
   }
-
-  const onClickModalClose = () => {
+  // 마커 생성 폼 모달 닫기 함수
+  const onClickCreateModalClose = () => {
     setAddressSearchOpen(false)
-    setIsActive(false)
+    setCreateModalActive(false)
   }
-
+  // 주소검색 버튼 클릭시 카카오 주소검색 오픈 함수
   const onClickSearch = () => {
     setAddressSearchOpen(true)
   }
-
+  // 주소 검색 완료후 이벤트
   const onCompletePost = (data: any) => {
     setAddressSearchOpen(false)
     let fullAddr = data.address
@@ -86,7 +81,7 @@ const WritePage = () => {
         extraAddr += extraAddr !== '' ? `, ${data.buildingName}` : data.buildingName
       }
       fullAddr += extraAddr !== '' ? ` (${extraAddr})` : ''
-      setTest(false)
+
       const geocoder = new window.kakao.maps.services.Geocoder()
       console.log(fullAddr)
 
@@ -95,26 +90,22 @@ const WritePage = () => {
         // 정상적으로 검색이 완료됐으면
         if (status === window.kakao.maps.services.Status.OK) {
           const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x)
-          console.log(coords)
+          // console.log(coords)
           onSetMapInputAddress(fullAddr)
           setMapObjData({
             ...mapObjData,
-            fullAddr,
             coords,
           })
         }
       })
     }
   }
-
-  const [images, setImages] = useState<any>([])
-
+  // 이미지 업로드 작업
   const onFileChange = useCallback(
     async e => {
       const files = e.target.files
       const formData = new FormData()
       formData.append('file', files[0])
-
       try {
         const res = await axios.post('/api/image/upload', formData)
         // console.log(res)
@@ -127,24 +118,21 @@ const WritePage = () => {
     },
     [images],
   )
-
+  // 이미지 삭제 모달 온,오프 상태값
   const [imageRemoveModalActive, imageRemoveModalActiveToggle] = useBoolean(false)
+  // 삭제할 이미지 아이디 값
   const [removeImageId, setRemoveImageId] = useState<string>('')
-
+  // 이미지 삭제 모달 오픈 함수
   const imageRemoveModalOpen = (id: string) => {
     setRemoveImageId(id)
     imageRemoveModalActiveToggle()
   }
-
+  // 이미지 삭제 이벤트
   const onClickImageRemoveEvent = async () => {
     setImages(images.filter((v: any) => v.id !== removeImageId))
     imageRemoveModalActiveToggle()
   }
-
-  const closeOverlay = () => {
-    console.log('11111')
-  }
-
+  // 마커 생성 이벤트
   const onClickMapSave = () => {
     if (mapObjData.fullAddr === '') {
       ErrorMessageOpen('주소를 입력해 주세요.')
@@ -167,15 +155,14 @@ const WritePage = () => {
       diaryTitle,
       images,
       contentText,
-      fullAddr: mapObjData.fullAddr,
+      fullAddr: mapInputAddress,
       position: { lng: mapObjData.coords.La, lat: mapObjData.coords.Ma },
     }
     console.log('data', data)
     // setMapList([...mapList, { content: '', position: { lng: 126.987024769656, lat: 37.5705611277251 } }])
     setMapList([...mapList, data])
     // setAddressSearchOpen(false)
-
-    onClickModalClose()
+    onClickCreateModalClose()
   }
 
   // 모달에 들어갈 데이터값
@@ -190,9 +177,6 @@ const WritePage = () => {
 
   // 다이어리 저장
   const onClickDiarySave = async () => {
-    console.log('aaaaa')
-    console.log(user)
-    console.log(mapList)
     try {
       const res = await axios.post('/api/diary', {
         user_id: user.data.id,
@@ -226,22 +210,6 @@ const WritePage = () => {
 
         {/*<div style={{ width: '100%', height: '100%' }} id="map" ref={mapRef} />*/}
       </MapContainer>
-      <ModalContainer isActive={imageRemoveModalActive} closeEvent={imageRemoveModalActiveToggle} maxWidth="500px">
-        <Card title="이미지 삭제">
-          <SelectContainerBox>
-            <p>이미지를 삭제 하시겠습니까?</p>
-            <span className="line" />
-            <div className="button_box">
-              <Button theme="tertiary" onClick={imageRemoveModalActiveToggle}>
-                취소
-              </Button>
-              <Button theme="secondary" onClick={onClickImageRemoveEvent}>
-                삭제
-              </Button>
-            </div>
-          </SelectContainerBox>
-        </Card>
-      </ModalContainer>
 
       <ModalContainer isActive={saveModalActive} closeEvent={saveModalActiveToggle} maxWidth="500px">
         <Card title="다이어리 저장">
@@ -258,7 +226,7 @@ const WritePage = () => {
         </Card>
       </ModalContainer>
 
-      <ModalContainer isActive={isActive} closeEvent={onClickModalClose} maxWidth="500px">
+      <ModalContainer isActive={createModalActive} closeEvent={onClickCreateModalClose} maxWidth="500px">
         <Card title="리스트 추가">
           <CardInputGroup>
             <div className="title">날짜 선택</div>
@@ -309,7 +277,7 @@ const WritePage = () => {
             </div>
           </CardInputGroup>
           <CardButtonGroup>
-            <Button onClick={onClickModalClose} theme="tertiary">
+            <Button onClick={onClickCreateModalClose} theme="tertiary">
               닫기
             </Button>
             <Button onClick={onClickMapSave}>저장</Button>
@@ -317,9 +285,26 @@ const WritePage = () => {
         </Card>
       </ModalContainer>
 
+      <ModalContainer isActive={imageRemoveModalActive} closeEvent={imageRemoveModalActiveToggle} maxWidth="500px">
+        <Card title="이미지 삭제">
+          <SelectContainerBox>
+            <p>이미지를 삭제 하시겠습니까?</p>
+            <span className="line" />
+            <div className="button_box">
+              <Button theme="tertiary" onClick={imageRemoveModalActiveToggle}>
+                취소
+              </Button>
+              <Button theme="secondary" onClick={onClickImageRemoveEvent}>
+                삭제
+              </Button>
+            </div>
+          </SelectContainerBox>
+        </Card>
+      </ModalContainer>
+
       <SlideModal isActive={slideModalActive} onClickModalClose={onClickSlideModalClose} modalData={modalData} />
 
-      <NavBar createModalOpen={onClickModalOpen} onClickSaveModalOpen={onClickSaveModalOpen} />
+      <NavBar createModalOpen={onClickCreateModalOpen} onClickSaveModalOpen={onClickSaveModalOpen} />
     </>
   )
 }
